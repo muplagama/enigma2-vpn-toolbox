f63acb343ba44cd8d2f469b425dcfee88994c1ad
muplagama <37286134+muplagama@users.noreply.github.com>
2023-04-18 12:10:12 +0200

    Update pre_up.sh

--- pre_up.sh
+++ pre_up.sh
@@ -17,6 +17,16 @@ if  [ -n "$binary" ]; then
         _mydefaultgw=$(grep default /etc/$binary/route_default | awk '/default/ {print $3}')
                 if [[ $binary == "openvpn" ]]; then
                         _endpoint=$(grep -i remote /etc/$binary/*.conf | grep -vE 'cert|-random' |awk -F 'remote' '{print$2}' |awk '{print $1}' | head -n 1)
+                        ip route add $local_dns via "$_mydefaultgw"
+                                if [[ $_endpoint =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                                        _ip=$_endpoint
+                                        else
+                                        _ip=$(nslookup -query=A "$_endpoint" $local_dns | grep Address | sed '/:53$/d' | sed s/^[^0-9]*// | head -n 1)
+                                        echo "hostname  resolved to: $_ip"
+                                fi
+                          route_set=$(ip route show | grep ${_ip})
+                                if [ -z "$route_set" ]; then
+                                ip route add ${_ip} via $_mydefaultgw
                 elif [[ $binary == "wireguard" ]]; then
                         _endpoint=$(grep Endpoint /etc/$binary/*.conf | awk -F'=' '{print $2}' | awk -F# '{gsub(/ /,"");print ($1) }' | awk -F ':' '{print $1}')
                         ip route add $local_dns via "$_mydefaultgw"
